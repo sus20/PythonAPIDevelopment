018cbb1650b95c1d2184ca260004bf2d636abe1b
sushma <a01363028@unet.univie.ac.at>
2023-07-03 14:45:59 +0200

    feat: updateAdded

--- main.py
+++ main.py
@@ -68,3 +68,18 @@ def delete_post(id: int):
                             detail=f"post with id :{id} doesn't exit")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id :{id} doesn't exit")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {'data': post_dict}
