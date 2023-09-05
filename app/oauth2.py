9c35637de3bdda89b07fd23dbb13157c1b545cd0
sushma <a01363028@unet.univie.ac.at>
2023-09-05 13:50:24 +0200

    feat: fetchingUserinProtectedRoutes

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
-from jose import JWSError, jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -12,7 +13,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 
 def create_access_token(data: dict):
@@ -30,18 +31,21 @@ def varify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
-
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
-    except JWSError:
+    except JWTError:
         raise credentials_exception
 
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+                                          detail=f"Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"})
 
-    return varify_access_token(token, credentials_exception)
+    token = varify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User. id == token.id).first()
+
+    return user
