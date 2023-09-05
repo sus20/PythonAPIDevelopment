16d588e52fb9d8697c5215a90b101c7c91e29a1f
sushma <a01363028@unet.univie.ac.at>
2023-09-05 15:46:00 +0200

    feat: DeleteUpdata&RetrievingLoggedinUsersPosts

--- app/routers/post.py
+++ app/routers/post.py
@@ -24,8 +24,8 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    print(current_user.email)
-    new_post = models.Post(**post.dict())
+
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -52,13 +52,18 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     #     """DELETE FROM posts WHERE id = %s returning * """, (str(id),))
     # deleted_post = cursor.fetchone()
     # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if post.first() == None:
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id :{id} doesn't exit.")
 
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -69,11 +74,18 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     #                       (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
     # conn.commit()
+
     post_query = db.query(models.Post).filter(models.Post.id == id)
+
     post = post_query.first()
+
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id :{id} doesn't exit")
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
 
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
