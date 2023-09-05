eae02b262949754d970a7779b45ad470a1e88bf4
sushma <a01363028@unet.univie.ac.at>
2023-09-05 18:25:22 +0200

    feat: addedDatabase.py&oauth2.pyEnvVariable

--- app/database.py
+++ app/database.py
@@ -2,12 +2,15 @@ from datetime import time
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from .config import settings
+
 
 # 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Postgres@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
