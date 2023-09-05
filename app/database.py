093b10819d812f7b9f551eebe1e19f00b86d917c
sushma <a01363028@unet.univie.ac.at>
2023-09-05 17:26:34 +0200

    fix: cleanupMain.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
+from datetime import time
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 # 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Postgres@localhost/fastapi'
@@ -18,3 +22,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi',
+#                                 user='postgres', password='Postgres',
+#                                 cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successful!")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from .import models
 from .database import engine
 from .routers import post, user, auth
@@ -8,41 +6,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# Schema Models/ Pydantic Models define the structure of a request and response
-# this ensure that when a user wants to create a post, the request will only go through
-# if it has a "title" and "content" in the body.
-
-
-while True:
-
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi',
-                                user='postgres', password='Postgres',
-                                cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-
-my_posts = [{"title": "title of post 1",
-             "content": "content of post 1", "id": 1},
-            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
 
 app.include_router(post.router)
 app.include_router(user.router)
