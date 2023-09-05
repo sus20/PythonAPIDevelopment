90de0b9f9b42797c49a523aa837ff079308ad402
sushma <a01363028@unet.univie.ac.at>
2023-09-05 17:08:48 +0200

    feat: queryParameterslimit&skipinGetposts

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,10 +11,14 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 2):
     # cursor.execute(""" SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+
+    print("limit:", limit)
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(
+    #     models.Post.owner_id == current_user.id).all()
     return posts
 
 
@@ -43,6 +47,9 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db), current
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id : {id} was not found.")
 
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
     return post
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -15,10 +15,20 @@ class PostCreate(PostBase):
 # Response
 
 
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
 class Post(PostBase):
     id: int
     created_at: datetime
     owner_id: int
+    owner: UserOut
 
     class Config:
         orm_mode = True
@@ -27,15 +37,6 @@ class Post(PostBase):
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
-
-
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
 
 
 class UserLogin(BaseModel):
