60e0253e6cc619235c0e4c07eb1c77a68985a8a0
sushma <a01363028@unet.univie.ac.at>
2023-09-06 12:00:15 +0200

    feat: addedPhnNumToPostsTable&createdAutoVote

--- app/models.py
+++ app/models.py
@@ -29,6 +29,7 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+    phone_number = Column(String)
 
 
 class Vote (Base):
