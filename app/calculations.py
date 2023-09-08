177f619d03bde3668b50d0edf5a5557afbdf9efc
sushma <a01363028@unet.univie.ac.at>
2023-09-08 21:05:40 +0200

    test: TestingBankAccountwithExceptions

--- app/calculations.py
+++ app/calculations.py
@@ -14,6 +14,10 @@ def divide(num1: int, num2: 2):
     return num1 / num2
 
 
+class InsufficientFunds(Exception):
+    pass
+
+
 class BankAccount():
     def __init__(self, starting_balance=0):
         self.balance = starting_balance
@@ -22,7 +26,9 @@ class BankAccount():
         self.balance += amount
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if amount > self.balance:
+            raise InsufficientFunds("Insufficient funds in account.")
+            self.balance -= amount
 
     def collect_interest(self):
         self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculations import add, subtract, multiply, divide, BankAccount
+from app.calculations import add, subtract, multiply, divide, BankAccount, InsufficientFunds
 
 
 @pytest.fixture
@@ -59,7 +59,17 @@ def test_collect_interest(bank_account):
     assert round(bank_account.balance, 6) == 55
 
 
-def test_bank_transaction(zero_bank_account):
-    zero_bank_account.deposit(200)
-    zero_bank_account.withdraw(100)
-    assert zero_bank_account.balance == 100
+@pytest.mark.parametrize("deposited, withdraw, expected", [
+    (200, 100, 100),
+    (50, 10, 40),
+    (1200, 200, 1000),
+])
+def test_bank_transaction(zero_bank_account, deposited, withdraw, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdraw)
+    assert zero_bank_account.balance == expected
+
+
+def test_insufficient_funds(bank_account):
+    with pytest.raises(InsufficientFunds):
+        bank_account.withdraw(200)
