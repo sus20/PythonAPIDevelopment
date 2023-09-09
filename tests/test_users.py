ecf912a59dd0ecef6a31baa0f416e02e421de42a
sushma <a01363028@unet.univie.ac.at>
2023-09-09 10:20:19 +0200

    test: created_test_creat_user()

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
@@ -8,3 +9,13 @@ def test_root():
     res = client.get("/")
     print(res.json().get('message'))
     assert res.json().get('message') == 'Hello World hurra!!!'
+    assert res.status_code == 200
+
+
+def test_creat_user():
+    res = client.post(
+        "/users", json={"email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
