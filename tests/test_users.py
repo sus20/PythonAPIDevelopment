78bb0830e1b32726517840a36807843e85c66879
sushma <a01363028@unet.univie.ac.at>
2023-09-09 15:37:50 +0200

    test: validate token

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,67 +1,36 @@
-from fastapi.testclient import TestClient
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.main import app
 from app import schemas
+from .database import client, session
+from jose import jwt
 from app.config import settings
-from app.database import get_db, Base
-from alembic import command
-
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)  # destroy a table
-    Base.metadata.create_all(bind=engine)  # create a table
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def test_user(client):
+    user_data = {"email": "sus@gmail.com", "password": "sus"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-
-
-def test_root(client, session):
-    res = client.get("/")
-    print(res.json().get('message'))
-    assert res.json().get('message') == 'Hello World hurra!!!'
-    assert res.status_code == 200
-
-
-def test_creat_user(client):
+def test_create_user(client):
     res = client.post(
-        "/users", json={"email": "hello123@gmail.com", "password": "password123"})
+        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
 
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(client, test_user):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(** res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
