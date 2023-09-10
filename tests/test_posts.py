3534c42344ec514f5cced8e5d7e5ad5704d964ca
sushma <a01363028@unet.univie.ac.at>
2023-09-10 12:31:26 +0200

    test: GetOnePostByID

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -18,3 +18,8 @@ def test_get_all_posts(authorized_client, test_posts):
 def test_unauthorized_user_get_all_posts(client, test_posts):
     res = client.get("/posts")
     assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
