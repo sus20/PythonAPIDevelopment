c47a717c99519a6a9ac81ebaa3d30f76114f81c2
sushma <a01363028@unet.univie.ac.at>
2023-09-10 13:17:05 +0200

    test: unauthorizedUserCreatePost

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -15,7 +15,7 @@ def test_get_all_posts(authorized_client, test_posts):
     assert res.status_code == 200
 
 
-def test_unauthorized_user_get_all_posts(client, test_posts):
+def test_unauthorized_user_get_all_posts(client):
     res = client.get("/posts")
     assert res.status_code == 401
 
@@ -25,7 +25,7 @@ def test_unauthorized_user_get_one_post(client, test_posts):
     assert res.status_code == 401
 
 
-def test_get_one_post_not_exist(authorized_client, test_posts):
+def test_get_one_post_not_exist(authorized_client):
     res = authorized_client.get(f"/posts/88888")
     assert res.status_code == 404
 
@@ -44,7 +44,7 @@ def test_get_one_post(authorized_client, test_posts):
     ("tallest skyscrapers", "wahoo", True)
 
 ])
-def test_create_post(authorized_client, test_user, test_posts, title, content, published):
+def test_create_post(authorized_client, test_user, title, content, published):
     res = authorized_client.post(
         "/posts/", json={"title": title, "content": content, "published": published})
 
@@ -56,7 +56,7 @@ def test_create_post(authorized_client, test_user, test_posts, title, content, p
     assert created_post.owner_id == test_user['id']
 
 
-def test_create_post_default_published_true(authorized_client, test_user, test_posts):
+def test_create_post_default_published_true(authorized_client, test_user):
     res = authorized_client.post(
         "/posts/", json={"title": "arbitrary title", "content": "arbitrary content"})
 
@@ -66,3 +66,9 @@ def test_create_post_default_published_true(authorized_client, test_user, test_p
     assert created_post.content == "arbitrary content"
     assert created_post.published == True
     assert created_post.owner_id == test_user['id']
+
+
+def test_unauthorized_user_create_post(client, test_posts):
+    res = client.post(
+        "/posts", json={"title": "arbitrary title", "content": "arbitrary content"})
+    assert res.status_code == 401
