acccc0bfafae53a7ab3c61a6b586371329923db6
sushma <a01363028@unet.univie.ac.at>
2023-09-10 12:35:54 +0200

    test: getOnePostNotexist&getOnePost

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -23,3 +23,13 @@ def test_unauthorized_user_get_all_posts(client, test_posts):
 def test_unauthorized_user_get_one_post(client, test_posts):
     res = client.get(f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/88888")
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    print(res.json())
