6c93a118966109c5aba5ddcdccf4acd5fe896fa9
sushma <a01363028@unet.univie.ac.at>
2023-09-10 12:26:34 +0200

    test: postsFixtureToCreateTestPosts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,15 @@
+from typing import List
+from app import schemas
+
+
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
-    print(res.json())
+
+    def validate(post):
+        return schemas.PostOut(**post)
+
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+
+    assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
