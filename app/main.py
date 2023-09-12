a0595a8353cb59c154c825a6df896b916506e44c
sushma <a01363028@unet.univie.ac.at>
2023-09-12 13:50:04 +0200

    fix: removedUnusedLibraries

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .import models
-from .database import engine
 from .routers import post, user, auth, vote
-from .config import settings
 
 # models.Base.metadata.create_all(bind=engine)
 
